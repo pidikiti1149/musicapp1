e96bff47fdc09890779cca31d27ce4857110b285
PrathimaGade <bprathimag@gmail.com>
2019-10-15 17:50:25 -0400

    frst commit

--- App.js
+++ App.js
@@ -2,12 +2,51 @@ import React from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import {Button} from 'react-native-elements';
 
-export default function App() {
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: null,
+    }
+  }
+  componentDidMount () {
+
+    return fetch('https://thawing-hollows-21222.herokuapp.com/artists.json')
+      .then( (response) => response.json() )
+      .then( (responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.artists,
+        })
+      })
+
+      .catch((error) => {
+        console.log(error)
+      });
+    }
+
+render() {
+  if(this.state.isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Content Loaded</Text>
+      </View>
+    )
+
+  }
   return (
     <View style={styles.container}>
       <Text>Welcome to MUSICFEST  app!</Text>
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
